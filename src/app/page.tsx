680c8fae6f3ddf0ca253b894db22b2484f0b1fef
Git4Prg <parigyan.sroy@gmail.com>
2026-01-23 21:02:06 +0530

    background jobs, inngest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,25 @@
+"use client"
+
 import { requireAuth } from "@/lib/auth-utils";
 import { caller } from "@/trpc/server";
 import { LogoutButton } from "./logout";
+import { useTRPC } from "@/trpc/client";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
 
-const Page = async () => {
-  await requireAuth();
+const Page = () => {
+  // await requireAuth();
 
-  const data = await caller.getUsers();
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  const { data } = useQuery(trpc.getWorkflows.queryOptions());
+
+  const create = useMutation(trpc.createWorkflow.mutationOptions({
+    onSuccess: () => {
+      toast.success("Job queued")
+    }
+  }));
 
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6">
@@ -13,6 +27,9 @@ const Page = async () => {
       <div>
         {JSON.stringify(data, null, 2)}
       </div>
+      <Button disabled={create.isPending} onClick={() => create.mutate()}>
+        Create Workflow
+      </Button>
       <LogoutButton />
     </div>
   )
