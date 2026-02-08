effe34e3f766a1241f0387312c9934e353c77a8d
Git4Prg <parigyan.sroy@gmail.com>
2026-02-08 11:47:26 +0530

    Node executions

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, {useState, useCallback} from 'react';
+import React, {useState, useCallback, useMemo} from 'react';
 import {
     ReactFlow,
     applyNodeChanges,
@@ -23,6 +23,8 @@ import { nodeComponents } from '@/config/node-components';
 import { AddNodeButton } from '@/features/editor/components/add-node-button';
 import { editorAtom } from '../store/atoms';
 import { useSetAtom } from 'jotai';
+import { NodeType } from '@/generated/prisma/enums';
+import { ExecuteWorkflowButton } from './execute-workflow-button';
 
 export const EditorLoading = () => {
     return <LoadingView message="Loading editor..." />
@@ -54,6 +56,10 @@ export const Editor = ({workflowId}: {workflowId: string}) => {
         [],
     );
 
+    const hasManualTrigger = useMemo(() => {
+        return nodes.some((node) => node.type === NodeType.MANUAL_TRIGGER); 
+    }, [nodes]);
+
     return (
         <div className='size-full'>
             <ReactFlow
@@ -77,6 +83,11 @@ export const Editor = ({workflowId}: {workflowId: string}) => {
                 <Panel position="top-right">
                     <AddNodeButton />
                 </Panel>
+                {hasManualTrigger && (
+                    <Panel position="bottom-center">
+                        <ExecuteWorkflowButton workflowId={workflowId} />
+                    </Panel>
+                )}
             </ReactFlow>
         </div>
     )
